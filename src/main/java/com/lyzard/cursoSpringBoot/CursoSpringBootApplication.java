824fc9a4431a3915d5551b74605ab9b5bd096b5d
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-17 14:35:09 -0300

    Project created

---
+++ src/main/java/com/lyzard/cursoSpringBoot/CursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lyzard.cursoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+}
