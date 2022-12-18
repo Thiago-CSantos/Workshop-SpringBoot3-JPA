d0eafcc6b9b2452e38ed1b3043b5fa590bdd954c
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-18 10:20:19 -0300

    H2 database, test profile, JPA

--- src/main/java/com/lyzard/cursoSpringBoot/entities/User.java
+++ src/main/java/com/lyzard/cursoSpringBoot/entities/User.java
@@ -1,22 +1,31 @@
 package com.lyzard.cursoSpringBoot.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public class User implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private Integer id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
 	}
 
-	public User(Integer id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -24,11 +33,11 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -66,7 +75,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -78,9 +90,12 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
+
 }
--- src/main/java/com/lyzard/cursoSpringBoot/resource/UserResource.java
+++ src/main/java/com/lyzard/cursoSpringBoot/resource/UserResource.java
@@ -14,7 +14,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Maria", "maria@gmail.com", "9999-9999", "123456");
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999-9999", "123456");
 		
 		return ResponseEntity.ok().body(u);
 		
