4f3182b9f1a87f8827f00cd4940a1ad337ca9af6
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-20 09:02:45 -0300

    OrderStatus enum

--- src/main/java/com/lyzard/cursoSpringBoot/config/TestConfig.java
+++ src/main/java/com/lyzard/cursoSpringBoot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.lyzard.cursoSpringBoot.entities.Order;
 import com.lyzard.cursoSpringBoot.entities.User;
+import com.lyzard.cursoSpringBoot.entities.enums.OrderStatus;
 import com.lyzard.cursoSpringBoot.repositories.OrderReoisitory;
 import com.lyzard.cursoSpringBoot.repositories.UserRepository;
 
@@ -28,11 +29,11 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
-		userRepo.saveAll(Arrays.asList(u1,u2)); 	//saveAll recebe uma list
+		userRepo.saveAll(Arrays.asList(u1,u2)); 	//saveAll recebe uma list como paramentro
 		orderRepo.saveAll(Arrays.asList(o1,o2,o3));
 	}
 }
--- src/main/java/com/lyzard/cursoSpringBoot/entities/Order.java
+++ src/main/java/com/lyzard/cursoSpringBoot/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.lyzard.cursoSpringBoot.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Order implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -30,10 +33,11 @@ public class Order implements Serializable{
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -51,6 +55,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOff(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
